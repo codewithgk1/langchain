bfedbf8322513ffd9cd5c31bc58dcb647eeefe89
mharkhani <mukesh.harkhani@gmail.com>
2025-02-10 15:45:24 +0530

    chat basics

--- 1_chat_models/1_chat_model_basic.py
+++ 1_chat_models/1_chat_model_basic.py
@@ -3,6 +3,10 @@ from langchain_openai import ChatOpenAI
 model = ChatOpenAI(model="gpt-4o-mini")
 result = model.invoke("Hello, how are you?")
 
-# print(result)
+print("Full response:")
+print(result)
 
+print("************")
+
+print("Content only:")
 print(result.content)
